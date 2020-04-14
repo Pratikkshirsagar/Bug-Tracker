313ddae2c42e32760f0ea36c5ed835f875188a16
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-14 22:02:25 +0530

    reducer created

---
+++ src/reducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = [];
+
+function reducer(state = INITIAL_STATE, action) {
+  if (action === 'bugAdded')
+    return [
+      ...state,
+      {
+        id: lastId++,
+        description: action.payload.description,
+        resolve: false,
+      },
+    ];
+  else if (action.type === 'bugRemoved') {
+    return state.filter((bug) => bug.id !== action.payload.id);
+  }
+
+  return state;
+}
