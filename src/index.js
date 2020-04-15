d8a457dd80756acf0b81a52fa01e19f0711d67a1
Pratikkshirsagar <meetpratiik@gmail.com>
2020-04-15 15:24:39 +0530

    ducks pattern added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import store from './store';
-import { bugAdded, bugRemove, bugResolved } from './actions';
+import store from './store/store';
+import { bugAdded, bugRemove, bugResolved } from './store/actions';
 
 store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(bugAdded(1, 'Bug 1'));
 store.dispatch(bugAdded(2, 'Bug 2'));
+store.dispatch(bugAdded(3, 'Bug 3'));
 
 // store.dispatch(bugRemove(1));
 
